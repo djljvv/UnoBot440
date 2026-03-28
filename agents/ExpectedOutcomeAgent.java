133d63a098ea23df2e6839300c8ff1a8df5e7a4d
djljvv <djlee05@bu.edu>
2026-03-28 03:12:09 -0400

    completed expected outcome agent

--- agents/ExpectedOutcomeAgent.java
+++ agents/ExpectedOutcomeAgent.java
@@ -65,9 +65,125 @@ public class ExpectedOutcomeAgent
     {
 
         // TODO: implement me!
+        //necessary for argmaxQValues, do not touch
         this.LastDrawnCardIdx = drawnCardIdx;
         this.PresentGameView = game;
-        return null;
+
+        //minimax from lab but edited given three things, initated below and then ran in helper
+        int CurrentPlayer = game.getPlayerOrder().getCurrentLogicalPlayerIdx();
+        //starting point for the tree based on the current player to max max max
+        Node root = new MCTSNode(game, CurrentPlayer, null);
+
+        minimaxedSearch(root);
+        
+
+        return root;
+    }
+
+    //helper recurive method for modded minimax
+    private float[] minimaxedSearch(Node node){
+        int numPlayers = this.getNumPlayers(node.getGameView());
+
+        //we're at a known terminal state so just return
+        if(node.isTerminal()){
+            //terminal state is when one player has no cards left, so we check for that and assign 1.0 to the winner and 0.0 to everyone else
+            float[] terminalScores = new float[numPlayers];
+
+            for(int i = 0; i < numPlayers; ++i){
+                if(node.getGameView().getHandView(i).size() == 0){
+                    terminalScores[i] = 1.0f;
+                }
+                else{
+                    terminalScores[i] = 0.0f;
+                }
+            }
+
+            return terminalScores;
+        }
+
+        //basically tells us given the node/player we are at, what are our possibilities
+        int numActions;
+        if(node.getNodeState() == Node.NodeState.HAS_LEGAL_MOVES){
+            numActions = node.getOrderedLegalMoves().size();
+        }
+        else if(node.getNodeState() == Node.NodeState.NO_LEGAL_MOVES_UNRESOLVED_CARDS_PRESENT){
+            numActions = 1;
+        }
+        else{
+            numActions = 2;
+        }
+
+        //what player are we maxing for given they are the parent
+        int currentPlayer = node.getLogicalPlayerIdx();
+        //best qvector retunred so far initalized(parent)
+        float[] bestScores = null;
+        //player component from best vector(parent)
+        float winningScore = Float.NEGATIVE_INFINITY;
+
+        //loops actions
+        for(int i = 0; i < numActions; ++i){
+            //build out the move
+            Move move = null;
+
+            //make hand given legal moves
+            if(node.getNodeState() == Node.NodeState.HAS_LEGAL_MOVES){
+                int cardIdx = node.getOrderedLegalMoves().get(i);
+                HandView hand = node.getGameView().getHandView(node.getLogicalPlayerIdx());
+                Card card = hand.getCard(cardIdx);
+                move = this.makeMoveFromCard(cardIdx, card, hand, null);
+            }
+            else if(node.getNodeState() == Node.NodeState.NO_LEGAL_MOVES_MAY_PLAY_DRAWN_CARD){
+                //if we wanna play
+                if(i == 0){
+                    move = Move.createMove(this, 0);
+                }
+            }
+
+            //parent "recurses" on child but ignore the fact we are iterating
+            Node child = node.getChild(move);
+            if(child == null){
+                continue;
+            }
+
+            //this is the q-vector for the child node, and we check the children of it
+            float[] childScores = minimaxedSearch(child);
+            //comparison value used by the parent, i.e. player one looks at its q values, from what is has what is best for it, do same for all other players who are at n given node
+            float thisPlayerScore = childScores[currentPlayer];
+
+            //update q-values in q-vector for the current node based on the child scores, this is where players have to "deal" with choices of others(from andrew woods office hours)
+            node.setQCount(i, node.getQCount(i) + 1);
+            node.setQValueTotal(i, node.getQValueTotal(i) + thisPlayerScore);
+
+            //best child from parents perspective, propogating up node values
+            if(bestScores == null || thisPlayerScore > winningScore){
+                bestScores = childScores;
+                winningScore = thisPlayerScore;
+            }
+        }
+
+        //no children works = this guy banned child labor
+        if(bestScores == null){
+            return new float[numPlayers];
+        }
+
+        //if you don't know what this means... dawg come on
+        return bestScores;
+    }
+
+    private int getNumPlayers(final GameView game){
+        int numPlayers = 0;
+
+        while(true){
+            try{
+                game.getHandView(numPlayers);
+                ++numPlayers;
+            }
+            catch(Exception e){
+                break;
+            }
+        }
+
+        return numPlayers;
     }
 
     /**
@@ -146,9 +262,6 @@ public class ExpectedOutcomeAgent
         else{
             throw new IllegalStateException("the result was not expected" + node.getNodeState());
         }
-
-
-        return null;
     }
 
     //created from insanity, I'm not going to lie I found random parts of the library and im assuming they hopefully proabably work
@@ -166,6 +279,49 @@ public class ExpectedOutcomeAgent
             wildColor = this.chooseWildColor(hand);
         }
         return Move.createMove(this, cardIdx, wildColor);
+    }
+
+    //color fix
+    private Color chooseWildColor(final HandView hand){
+        int redCount = 0;
+        int asianCount = 0;
+        //what color are asians
+        int greenCount = 0;
+        int blueCount = 0;
+
+        for(int i = 0; i < hand.size(); ++i){
+            Card card = hand.getCard(i);
+
+            if(card.color() == Color.RED){
+                ++redCount;
+            }
+            else if(card.color() == Color.YELLOW){
+                ++asianCount;
+            }
+            else if(card.color() == Color.GREEN){
+                ++greenCount;
+            }
+            else if(card.color() == Color.BLUE){
+                ++blueCount;
+            }
+        }
+
+        Color bestColor = Color.RED;
+        int bestCount = redCount;
+
+        if(asianCount > bestCount){
+            bestColor = Color.YELLOW;
+            bestCount = asianCount;
+        }
+        if(greenCount > bestCount){
+            bestColor = Color.GREEN;
+            bestCount = greenCount;
+        }
+        if(blueCount > bestCount){
+            bestColor = Color.BLUE;
+        }
+
+        return bestColor;
     }
 
 }
